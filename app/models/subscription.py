1f3f86019e7683c2923b7b2a327a9d1be9e77dc0
luka <edulukalortk@gmail.com>
2024-09-03 18:40:05 +0000

    sub schema

--- app/models/subscription.py
+++ app/models/subscription.py
@@ -1,15 +1,29 @@
 from pydantic import BaseModel, Field
 from bson import ObjectId
 from datetime import datetime
-from .server import PyObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 class SubscriptionModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     user_id: str
-    server_id: PyObjectId
     start_date: datetime
     next_billing_date: datetime
     paypal_agreement_id: str
+    status: str = "pending"  # pending, active, cancelled, expired
 
     class Config:
         allow_population_by_field_name = True
