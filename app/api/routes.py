f0faa7bfe2b2187f46012fcd6beead1ba4d0ffdd
luka <edulukalortk@gmail.com>
2025-07-06 15:44:17 +0400

    repo refactor

--- app/api/routes.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from app.api.endpoints import subscription
-
-router = APIRouter()
-router.include_router(subscription.router, prefix="/subscriptions", tags=["subscriptions"])
